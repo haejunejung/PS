8cd6ba7347889a6f14c66dfbd8a50438eb6559a0
haejunejung <99087502+haejunejung@users.noreply.github.com>
2023-09-03 10:59:33 +0900

    [Silver I] Title: 회의실 배정, Time: 40 ms, Memory: 8684 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -6,43 +6,6 @@
 // 각 회의가 겹치지 않게 하면서 회의실을 사용할 수 있는 최대 개수를 찾아라.
 using namespace std;
 
-void merge_sort (vector<vector<int>> &meetings, const int l, const int r) {
-    const int m = (l + r) / 2;
-
-    vector<vector<int>> temp(meetings.size());
-
-    int i = l;
-    int j = m + 1;
-    int k = l;
-
-    while (i <= m && j <= r) {
-        if (meetings[i][1] <= meetings[j][1]) {
-            temp[k++] = meetings[i++];
-        }
-        else {
-            temp[k++] = meetings[j++];
-        }
-    }
-
-    int tmp = (i > m) ? j : i;
-    while (k <= r) {
-        temp[k++] = meetings[tmp++];
-    }
-
-    for (int i = l; i <= r; i++) {
-        meetings[i] = temp[i];
-    }
-}
-
-void merge (vector<vector<int>> &meetings, const int l, const int r) {
-    if (l < r) {
-        const int m = (l + r) / 2;
-        merge(meetings, l, m);
-        merge(meetings, m+1, r);
-        merge_sort (meetings, l, r);
-    }
-}
-
 int main () {
 
     std::ios_base::sync_with_stdio(false);
@@ -66,8 +29,6 @@ int main () {
 
         return a[1] < b[1];
     });
-
-    // merge(meetings, 0, meetings.size() - 1);
 
     int answer = 1;
     s = meetings[0][0];
